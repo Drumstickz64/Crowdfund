e21bb267910db4c785b1dba01f71d1d9b1ce5dbd
Drumstickz64 <gearman533@gmail.com>
2021-03-25 16:48:10 +0300

    added a store object to store and manage the entire state of the app

---
+++ src/js/Store.js
@@ -0,0 +1,41 @@
+const Store = {
+	/**
+		a single Store object to manage the entire state of the app
+	*/
+	_state: {
+		donationTarget: 100000,
+		amountDonated: 89914,
+		numOfBackers: 5007,
+	},
+	
+	_events: {},
+	
+	subscribe(stateVar, callback) {
+		if (!this._events[stateVar]) this._events[stateVar] = [];
+		
+		return this._events[stateVar].push(callback);
+	},
+	
+	getState(stateVar) {
+		return this._state[stateVar];
+	},
+	
+	setState(stateVar, newValue) {
+		this._state[stateVar] = newValue;
+		
+		this.executeCallbacks(stateVar, newValue);
+	},
+	
+	updateState(stateVar, modifierFunc) {
+		const oldValue = this.getState(stateVar);
+		const newValue = modifierFunc(oldValue);
+		
+		this.setState(stateVar, newValue);
+	},
+	
+	executeCallbacks(stateVar, value) {
+		this._events[stateVar].forEach(callback => {
+			callback(value);
+		});
+	}
+};
