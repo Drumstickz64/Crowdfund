68a9a46286db249bb807299b6db37164c77e41dd
Drumstickz64 <gearman533@gmail.com>
2021-03-25 23:19:58 +0300

    added a continue button to the no reward pledge that just increments the backer count

--- src/js/components/pledge-modal.js
+++ src/js/components/pledge-modal.js
@@ -19,13 +19,20 @@ function selectPledge(e) {
 function confirmPledge(e) {
 	const currentPledgeCard = e.target.closest(".pledge-card");
 	const inputElem = currentPledgeCard.querySelector(".pledge-card-input input");
-	let pledgeValue = Number(inputElem.value);
-	// payment is simply rounded down. this would not be the case in a real app
-	pledgeValue = Math.floor(pledgeValue);
 	
-	if (pledgeValue < 0) pledgeValue = 0;
+	// if inputElem exists (this wasn't the no reward pledge)
+	// then take pledge value and add it to the current amountPledged
+	if (inputElem) {
+		let pledgeValue = Number(inputElem.value);
+		
+		// payment is simply rounded down. this would not be the case in a real app
+		pledgeValue = Math.floor(pledgeValue);
+		
+		if (pledgeValue < 0) pledgeValue = 0;
+		
+		Store.updateState("amountPledged", oldValue => oldValue + pledgeValue);
+	}
 	
-	Store.updateState("amountPledged", oldValue => oldValue + pledgeValue);
 	Store.updateState("numOfBackers", oldValue => oldValue + 1);
 	
 	showPledgeCompleteModal();
