347a5079c23f2ee6e3f6467897cb58b2ee5f46da
Drumstickz64 <gearman533@gmail.com>
2021-03-27 21:44:37 +0300

    made some tweaks and added the ability to bookmark the page

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -2,4 +2,14 @@ import { showPledgeModal } from "./modal";
 
 
 const backProjectBtn = document.getElementById("back-project-btn");
+const bookmarkBtn = document.getElementById("bookmark-btn");
+const bookmarkIcon = document.getElementById("bookmark-icon");
+
+
+function toggleBookmark() {
+	bookmarkBtn.toggleAttribute("data-bookmarked");
+}
+
+
 backProjectBtn.addEventListener("click", showPledgeModal);
+bookmarkBtn.addEventListener("click", toggleBookmark);
